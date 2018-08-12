4d34c4a0380d527b4f97f8f9c9edec0255f62dea
Mihai Szilagyi <mihai.szilagyi@gmail.com>
2018-08-12 22:42:53 +0300

    format set5

---
+++ js/set/set5.js
@@ -0,0 +1,49 @@
+ function playPeste()
+{
+   var audio = document.getElementById("audioPeste");
+   audio.play();
+}
+function playBroasca()
+{
+   var audio = document.getElementById("audioBroasca");
+   audio.play();
+}
+function playCana()
+{
+   var audio = document.getElementById("audioCana");
+   audio.play();
+}
+
+function playPisica()
+{
+   var audio = document.getElementById("audioPisica");
+   audio.play();
+}
+
+function playSarpe()
+{
+   var audio = document.getElementById("audioSarpe");
+   audio.play();
+}
+
+function playCopac()
+{
+   var audio = document.getElementById("audioCopac");
+   audio.play();
+}
+
+function playBanana()
+{
+   var audio = document.getElementById("audioBanana");
+   audio.play();
+}
+    function playSoarece()
+{
+   var audio = document.getElementById("audioSoarece");
+   audio.play();
+}
+
+
+$(document).ready(function(){
+    console.log("active jquery")
+})
